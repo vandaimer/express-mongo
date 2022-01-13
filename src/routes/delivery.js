a53dc183a2fd99a481edf63c41deddf65743f42f
Luiz Filipe <moresco.luiz@gmail.com>
2022-01-13 20:39:38 +0100

    Use Errors constant on routes/Delivery

--- src/routes/delivery.js
+++ src/routes/delivery.js
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 
 import Delivery from '../core/Delivery';
+import { Errors } from './';
 
 const router = Router();
 
 router.post('/', async (req, res) => {
+  const defaultResponse = {
+    code: '',
+    msg: '',
+    result: []
+  };
+
   try {
     const result = Delivery.getDeliveries(req);
     return res.status(200).json(result);
-  } catch ({ message }) {
-    return res.status(400).json({ message });
+  } catch ({ message: msg }) {
+    const response = { ...defaultResponse, code: Errors.INVALID_PAYLOAD, msg };
+    return res.status(400).json(response);
   }
 });
 
