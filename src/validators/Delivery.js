e8edd419b07a5845954daf52c2bb992dc79036d6
Luiz Filipe <moresco.luiz@gmail.com>
2022-01-13 20:21:57 +0100

    Improve DeliveryValidator

--- src/validators/Delivery.js
+++ src/validators/Delivery.js
@@ -4,8 +4,8 @@ export default class Delivery {
   static genericSchema() {
     return Joi.object()
       .keys({
-        startDate: Joi.date().required(),
-        endDate: Joi.date().required(),
+        startDate: Joi.date().format('YYYY.MM.DD').required(),
+        endDate: Joi.date().format('YYYY.MM.DD').required(),
         minCount: Joi.number().integer().required(),
         maxCount: Joi.number().integer().required(),
       });
