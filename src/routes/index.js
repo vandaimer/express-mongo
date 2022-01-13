f2c347ba01393920c6fd43c5ce11a031c8d57535
Luiz Filipe <moresco.luiz@gmail.com>
2022-01-13 20:41:28 +0100

    Expose Errors from routes/Errors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import delivery from './delivery';
+import Errors from './errors';
 
 const router = Router();
 
 router.use('/delivery', delivery);
 
+export { Errors };
 export default router;
