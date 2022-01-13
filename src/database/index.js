d8f53b60daa2e6b6fac45bdefc1c92ef131cecd2
Luiz Filipe <moresco.luiz@gmail.com>
2022-01-13 21:21:00 +0100

    Implement connectDB func

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,11 @@
+import { MongoClient } from 'mongodb';
+
 import DeliveryCollection from './Delivery';
 
+const connectDB = (mongoURI) => {
+  const client = new MongoClient(mongoURI);
+  return client.connect();
+}
+
+export { connectDB };
 export { DeliveryCollection };
