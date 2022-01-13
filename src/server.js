2013775f6085dcaaabdfe873549b88d13aae2f88
Luiz Filipe <moresco.luiz@gmail.com>
2022-01-13 21:21:35 +0100

    Test mongodb connect during the start up

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import api from './express/api';
+import { connectDB } from './database';
 
 const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI || '';
 
-api(port)
+connectDB(mongoURI)
+  .then(client => {
+    client.close();
+    return api(port);
+  })
   .then(() => {
     // eslint-disable-next-line no-console
     console.info(`Server running. See http://localhost:${port}`);
